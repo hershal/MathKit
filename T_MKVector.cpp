ecbcab8fa52bedb0ea38e9e452822da7a1d2d55f
Hershal Bhave <hershal.bhave@gmail.com>
2014-07-10 00:57:23 -0700

    t_mkvector test is slightly fleshed out

--- T_MKVector.cpp
+++ T_MKVector.cpp
@@ -1,18 +1,40 @@
 // MathKit Vector Testing
 
 #include <iostream>
-#include <memory>
 #include <vector>
+#include <sstream>
+#include <random>
+#include <ctime>
+
+#include <boost/assert.hpp>
 
 #include "MKVector.hpp"
 
+void run_test();
+
 int main() {
-    
-    // std::shared_ptr<MKVector<double> > v(std::make_shared<MKVector<double> >(3));
-    MKVector<double> v(3);
-    v(0) = 0;
-    v(1) = 1;
-    v(2) = 2;
-    
-    std::cout << v.to_string() << std::endl;
+    std::size_t len = 10;
+    for (auto i=0; i<len; ++i) {
+        run_random_insertion_test();
+    }
+}
+
+void run_random_insertion_test() {
+    std::srand(std::time(0));
+
+    std::size_t len = (rand()%10000);
+    std::size_t num_insertions = (rand()%100000);
+
+    MKVector<double> v(len);
+
+    for (std::size_t i=0; i<num_insertions; ++i) {
+        std::size_t loc = (rand()%len + len/2);
+        double value = (double)(rand());
+        try {
+            v(loc) = value;
+            BOOST_ASSERT_MSG(v(loc) == value, (std::stringstream() << value << "!=" << v(i)).str().c_str());
+        } catch (std::out_of_range e) {
+            BOOST_ASSERT_MSG(loc >= len, (std::stringstream() << loc << ">=" << len).str().c_str());
+        }
+    }
 }
