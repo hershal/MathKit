1eefd4f07dd48ca4cce3b58c2a2dc67df4f1bbf6
Hershal Bhave <hershal.bhave@gmail.com>
2014-07-13 02:36:23 -0700

    made test framework slightly nicer

--- T_MKVector.cpp
+++ T_MKVector.cpp
@@ -19,7 +19,9 @@ typedef std::shared_ptr< std::map<std::string, std::size_t> > attribute_t_ptr;
 
 attribute_t_ptr accumulate_results(attribute_t_ptr addend, attribute_t_ptr augend);
 attribute_t_ptr assemble_test_results(std::size_t hits, std::size_t misses, std::size_t total);
-attribute_t_ptr run_random_insertion_test_out_of_bounds();
+attribute_t_ptr run_random_insertion_test_out_of_bounds(std::size_t vector_len,
+                                                        std::size_t num_insertions,
+                                                        float percentage_out_of_bounds);
 
 std::string string_results(std::string sep1, std::string sep2, attribute_t_ptr attrib);
 
@@ -28,33 +30,36 @@ int main() {
     auto tot = std::make_shared<attribute_t>();
 
     for (auto i=0; i<len; ++i) {
-        tot = accumulate_results(run_random_insertion_test_out_of_bounds(), tot);
+        tot = accumulate_results(run_random_insertion_test_out_of_bounds(10000,
+                                                                         100000,
+                                                                         0.25),
+                                 tot);
     }
 
     std::cout << string_results("=", "\n", tot) << std::endl;
 }
 
-attribute_t_ptr run_random_insertion_test_out_of_bounds() {
-    std::srand(std::time(0));
+attribute_t_ptr run_random_insertion_test_out_of_bounds(std::size_t vector_len,
+                                                        std::size_t num_insertions,
+                                                        float percentage_out_of_bounds) {
 
-    std::size_t len = (10000);
-    std::size_t num_insertions = 100000;
+    std::srand(std::time(0));
     std::size_t num_hits = 0;
     std::size_t num_misses = 0;
 
-    MKVector<double> v(len);
+    MKVector<double> v(vector_len);
 
     for (std::size_t i=0; i<num_insertions; ++i) {
-        std::size_t loc = ((rand()%len) + len/4);
-        double value = (double)(rand());
+        std::size_t loc = (std::size_t)((rand()%vector_len) + (vector_len*percentage_out_of_bounds));
+        float value = (float)(rand());
         try {
             v(loc) = value;
             BOOST_ASSERT_MSG(v(loc) == value,
                              (std::stringstream() << value << "!=" << v(i)).str().c_str());
             num_hits++;
         } catch (std::out_of_range e) {
-            BOOST_ASSERT_MSG(loc >= len,
-                             (std::stringstream() << loc << ">=" << len).str().c_str());
+            BOOST_ASSERT_MSG(loc >= vector_len,
+                             (std::stringstream() << loc << ">=" << vector_len).str().c_str());
             num_misses++;
         }
     }
