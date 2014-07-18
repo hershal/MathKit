d355daa820b21891c2ff514dfca698d08b1a77c2
Hershal Bhave <hershal.bhave@gmail.com>
2014-07-18 01:04:38 -0700

    added fill test to mkvector

--- T_MKMatrix.cpp
+++ T_MKMatrix.cpp
@@ -22,6 +22,14 @@ int main() {
     std::size_t insertion_num_insertions = (float)(pow(100000.0, 0.5));
     float insertion_percentage_out_of_bounds = 0.25;
 
+    /* Fill Test */
+    auto fill_test_results = TKNumberedAttribute<std::size_t>();
+    for (auto i=0; i<len; ++i) {
+        fill_test_results += *(fill_test(vector_len));
+    }
+    std::cout << "fill test\n" <<
+        fill_test_results.to_string() << "\n" << std::endl;
+
     /* Insertion Test */
     auto insertion_test_results = TKNumberedAttribute<std::size_t>();
     for (auto i=0; i<len; ++i) {
@@ -221,6 +229,42 @@ auto fused_arithmetic_assignment_test(std::size_t vector_len)
                  .str().c_str());
         }
     }
+    /* Generate result receipt */
+    auto m = std::make_shared<TKNumberedAttribute<std::size_t> >();
+    (*m)[k_num_total] = num_total;
+    return m;
+}
+
+auto fill_test(std::size_t vector_len)
+    -> std::shared_ptr<TKNumberedAttribute<std::size_t> > {
+
+    std::srand(std::time(0));
+    std::size_t num_total = 0;
+
+    /* Create the vectors */
+    MKVector < MKVector<float> > v1(vector_len, MKVector<float>(vector_len));
+    MKVector < MKVector<float> > v2(vector_len, MKVector<float>(vector_len));
+    std::vector<float> fill_value(vector_len);
+
+    /* Populate the vector values */
+    for (auto i=0; i<vector_len; ++i) {
+        fill_value[i] = (float)(rand());
+        v1[i] = fill_value[i];
+        for (auto j=0; j<vector_len; ++j) {
+            v2[i][j] = fill_value[i];
+            ++num_total;
+        }
+    }
+
+    /* Verify output */
+    for (auto i=0; i<vector_len; ++i) {
+        for (auto j=0; j<vector_len; ++j) {
+            BOOST_ASSERT_MSG(((v1[i][j] == v2[i][j]) && (v1[i][j] == fill_value[i])),
+                             (std::stringstream() << fill_value[i] << "!="<< v1[i][j] << "!=" << v2[i][j])
+                             .str().c_str());
+        }
+    }
+
     /* Generate result receipt */
     auto m = std::make_shared<TKNumberedAttribute<std::size_t> >();
     (*m)[k_num_total] = num_total;
