701509c4273eb44c0fa5d81dda96816376e57102
Hershal Bhave <hershal.bhave@gmail.com>
2014-07-19 19:49:42 -0700

    added mkgausselim header

---
+++ MKGaussElim.hpp
@@ -0,0 +1,17 @@
+/* MathKit Gaussian Elimination Algorithm Header */
+
+#ifndef MK_GAUSS_ELIM_HPP
+#define MK_GAUSS_ELIM_HPP
+
+#include <memory>
+#include "MKMatrix.hpp"
+
+typedef MKVector_p std::shared_ptr<MKVector <float> >;
+
+auto MKGaussPivotIndex(MKMatrix_p A, int k, int n) -> int;
+auto MKGaussRowExchange(MKMatrix_p A, MKVector_p b, int k, int p, int n) -> void;
+auto MKGaussFwdElimination(MKMatrix_p A, MKVector_p b, int k, int n) -> void;
+auto MKGaussBwdSubstitution(MKMatrix_p A, MKVector_p b, MKVector_p x, int n) -> void;
+auto MKGaussElim(MKMatrix_p A, MKVector_p b, MKVector_p x) -> int;
+
+#endif // MK_GAUSS_ELIM_HPP
