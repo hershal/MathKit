5bdee88aa7e4343e144ba22b42e762a7687efc02
Hershal Bhave <hershal.bhave@gmail.com>
2014-07-21 22:54:13 -0700

    implemented row pivoting

---
+++ MKGaussElim.cpp
@@ -0,0 +1,32 @@
+/* MathKit Gaussian Elimination Implemenation */
+
+#include "MKGaussElim.hpp"
+#include "MKFloatLib.hpp"
+
+/*! \brief Searches for an appropriate pivot for Gaussian Elimination
+*/
+auto MKGaussPivotIndex(MKMatrix_p A, int k, int n) -> int {
+    std::size_t i = k;
+    while ((i<n) && MFAlmostEqualRelative((*A)[i][k], 0.0)) {
+        ++i;
+    }
+
+    return i;
+}
+
+auto MKGaussRowExchange(MKMatrix_p A, MKVector_p b, int k, int p, int n) -> void {
+
+}
+
+auto MKGaussFwdElimination(MKMatrix_p A, MKVector_p b, int k, int n) -> void {
+
+}
+
+auto MKGaussBwdSubstitution(MKMatrix_p A, MKVector_p b, MKVector_p x, int n) -> void {
+
+}
+
+auto MKGaussElim(MKMatrix_p A, MKVector_p b, MKVector_p x) -> int {
+
+}
+
