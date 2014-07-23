f98367ed882bde5143146eb3d37e170a7f4b71e6
Hershal Bhave <hershal.bhave@gmail.com>
2014-07-22 23:05:59 -0700

    mkgausseslim test now slightly more verbose

--- T_MKGaussElim.cpp
+++ T_MKGaussElim.cpp
@@ -35,16 +35,29 @@ int main() {
     (*mat)[2][1] = 32;
     (*mat)[2][2] = 33;
 
+    std::cout << "original matrix" << std::endl;
     std::cout << (*mat).to_string("\n") << std::endl;
+    std::cout << "original vector" << std::endl;
     std::cout << (*vec).to_string(", ") << std::endl;
 
+    std::cout << std::endl;
+
+    std::cout << "pivots at each row" << std::endl;
     std::cout << MKGaussPivotIndex(mat, 0) << std::endl;
     std::cout << MKGaussPivotIndex(mat, 1) << std::endl;
     std::cout << MKGaussPivotIndex(mat, 2) << std::endl;
 
-    MKGaussRowExchange(mat, vec, 1, 2);
+    std::cout << std::endl;
 
+    auto row_exchange_1 = 1;
+    auto row_exchange_2 = 2;
+    MKGaussRowExchange(mat, vec, row_exchange_1, row_exchange_2);
+    std::cout << "after exchanging row " << row_exchange_1
+              << " and " << row_exchange_2 << std::endl;
+    
+    std::cout << "exchanged matrix" << std::endl;
     std::cout << (*mat).to_string("\n") << std::endl;
+    std::cout << "exchanged vector" << std::endl;
     std::cout << (*vec).to_string(", ") << std::endl;
 
     std::cout << "test completed" << std::endl;
