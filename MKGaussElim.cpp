fc9685ab418e21c9359142d5a54b9f559bda324a
Hershal Bhave <hershal.bhave@gmail.com>
2014-07-22 22:55:35 -0700

    stop using int!

--- MKGaussElim.cpp
+++ MKGaussElim.cpp
@@ -9,7 +9,7 @@ const float epsilon = pow(10, -6);
 
 /*! \brief Searches for an appropriate pivot for Gaussian Elimination
 */
-auto MKGaussPivotIndex(MKMatrix_p A, int k) -> int {
+auto MKGaussPivotIndex(MKMatrix_p A, std::size_t k) -> std::size_t {
     std::size_t i = k;
     std::size_t n = (*A).size();
     while ((i<n) && FLAlmostEqualRelative((*A)[i][k], 0.0, epsilon)) {
@@ -19,7 +19,7 @@ auto MKGaussPivotIndex(MKMatrix_p A, int k) -> int {
     return i;
 }
 
-auto MKGaussRowExchange(MKMatrix_p A, MKVector_p b, int k, int p) -> void {
+auto MKGaussRowExchange(MKMatrix_p A, MKVector_p b, std::size_t k, std::size_t p) -> void {
     MKVector<float> tmp_A_val = (*A)[k];
     float tmp_b_val = (*b)[k];
     
@@ -30,15 +30,15 @@ auto MKGaussRowExchange(MKMatrix_p A, MKVector_p b, int k, int p) -> void {
     (*A)[p] = tmp_A_val;
 }
 
-auto MKGaussFwdElimination(MKMatrix_p A, MKVector_p b, int k, int n) -> void {
+auto MKGaussFwdElimination(MKMatrix_p A, MKVector_p b, std::size_t k) -> void {
 
 }
 
-auto MKGaussBwdSubstitution(MKMatrix_p A, MKVector_p b, MKVector_p x, int n) -> void {
+auto MKGaussBwdSubstitution(MKMatrix_p A, MKVector_p b, MKVector_p x, std::size_t n) -> void {
 
 }
 
-auto MKGaussElim(MKMatrix_p A, MKVector_p b, MKVector_p x) -> int {
+auto MKGaussElim(MKMatrix_p A, MKVector_p b, MKVector_p x) -> std::size_t {
     return 0;
 }
 
--- MKGaussElim.hpp
+++ MKGaussElim.hpp
@@ -8,10 +8,10 @@
 
 typedef std::shared_ptr<MKVector <float> > MKVector_p;
 
-auto MKGaussPivotIndex(MKMatrix_p A, int k) -> int;
-auto MKGaussRowExchange(MKMatrix_p A, MKVector_p b, int k, int p) -> void;
-auto MKGaussFwdElimination(MKMatrix_p A, MKVector_p b, int k, int n) -> void;
-auto MKGaussBwdSubstitution(MKMatrix_p A, MKVector_p b, MKVector_p x, int n) -> void;
-auto MKGaussElim(MKMatrix_p A, MKVector_p b, MKVector_p x) -> int;
+auto MKGaussPivotIndex(MKMatrix_p A, std::size_t k) -> std::size_t;
+auto MKGaussRowExchange(MKMatrix_p A, MKVector_p b, std::size_t k, std::size_t p) -> void;
+auto MKGaussFwdElimination(MKMatrix_p A, MKVector_p b, std::size_t k) -> void;
+auto MKGaussBwdSubstitution(MKMatrix_p A, MKVector_p b, MKVector_p x, std::size_t n) -> void;
+auto MKGaussElim(MKMatrix_p A, MKVector_p b, MKVector_p x) -> std::size_t;
 
 #endif // MK_GAUSS_ELIM_HPP
