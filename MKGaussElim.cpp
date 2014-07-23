559076c21a9ff4d16e2ff895814849b5d408e3fa
Hershal Bhave <hershal.bhave@gmail.com>
2014-07-22 23:24:18 -0700

    minor style changes

--- MKGaussElim.cpp
+++ MKGaussElim.cpp
@@ -11,7 +11,7 @@ const float epsilon = pow(10, -6);
 */
 auto MKGaussPivotIndex(MKMatrix_p A, std::size_t k) -> std::size_t {
     std::size_t i = k;
-    std::size_t n = (*A).size();
+    std::size_t n = A->size();
     while ((i<n) && FLAlmostEqualRelative((*A)[i][k], 0.0, epsilon)) {
         ++i;
     }
@@ -22,14 +22,18 @@ auto MKGaussPivotIndex(MKMatrix_p A, std::size_t k) -> std::size_t {
 auto MKGaussRowExchange(MKMatrix_p A, MKVector_p b, std::size_t k, std::size_t p) -> void {
     MKVector<float> tmp_A_val = (*A)[k];
     float tmp_b_val = (*b)[k];
-    
+
     (*b)[k] = (*b)[p];
     (*b)[p] = tmp_b_val;
 
     (*A)[k] = (*A)[p];
     (*A)[p] = tmp_A_val;
 }
+/*! \brief Utilizes forward-elimination techniques to create an
+  upper-triangular matrix.
 
+  /param k the row index at which to start the forward elimination
+ */
 auto MKGaussFwdElimination(MKMatrix_p A, MKVector_p b, std::size_t k) -> void {
 
 }
