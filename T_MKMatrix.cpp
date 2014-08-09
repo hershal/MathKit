ce467f48f82169da4527c3a51844c6b2a6dc9a5f
Hershal Bhave <hershal.bhave@gmail.com>
2014-08-09 00:23:23 -0700

    changed data type to be more configurable

--- T_MKMatrix.cpp
+++ T_MKMatrix.cpp
@@ -12,40 +12,36 @@
 
 #include "T_MKVector.hpp"
 
-auto fill_test(std::size_t mat_xy)
-    -> std::shared_ptr<TKCumulableAttribute<std::size_t> >;
+typedef float float_data_type_t;
 
 int main () {
     std::size_t len = 100;
-    std::size_t mat_xy = (float)(pow(10000.0, 0.5));
-
-    std::size_t insertion_num_insertions = (float)(pow(100000.0, 0.5));
-    float insertion_percentage_out_of_bounds = 0.25;
+    std::size_t mat_xy = pow(10000.0, 0.5);
 
     /* Fill Test */
     auto fill_test_results = TKCumulableAttribute<std::size_t>();
     for (auto i=0; i<len; ++i) {
         fill_test_results += *(fill_test(mat_xy));
     }
-    /* std::cout << "fill test\n" << */
-    /*     fill_test_results.to_string() << "\n" << std::endl; */
+    std::cout << "fill test\n" <<
+        fill_test_results.to_string() << "\n" << std::endl;
 
 }
 
-auto fill_test(std::size_t mat_xy)
-    -> std::shared_ptr<TKCumulableAttribute<std::size_t> > {
+auto fill_test(const std::size_t mat_xy)
+    -> const std::shared_ptr<TKCumulableAttribute<std::size_t> > {
 
     std::srand(std::time(0));
     std::size_t num_total = 0;
 
     /* Create the matrix */
-    MKMatrix<float> m1(mat_xy);
-    MKMatrix<float> m2(mat_xy);
-    std::vector<float> fill_value(mat_xy);
+    MKMatrix<float_data_type_t> m1(mat_xy);
+    MKMatrix<float_data_type_t> m2(mat_xy);
+    std::vector<float_data_type_t> fill_value(mat_xy);
 
     /* Populate the matrix values */
     for (auto i=0; i<mat_xy; ++i) {
-        fill_value[i] = (float)(rand());
+        fill_value[i] = (float_data_type_t)(rand()%mat_xy);
         m1[i] = fill_value[i];
         for (auto j=0; j<mat_xy; ++j) {
             m2[i][j] = fill_value[i];
